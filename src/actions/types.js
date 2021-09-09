5fb0d03c32fb6d6071f6a817469dcff24b56d5fb
anesboz <anesbouzouaoui@gmail.com>
2021-09-09 23:03:44 +0200

    Thu Sep  9 23:03:44 CEST 2021

--- src/actions/types.js
+++
@@ -1,9 +0,0 @@
-export const ATTRIBUTE_ONGLET = "ATTRIBUTE_ONGLET";
-export const RESET_ALL = "RESET_ALL";
-export const UPLOAD_PAGE = "UPLOAD_PAGE";
-export const GO_PAGE = "GO_PAGE";
-export const RESET_PAGE = "RESET_PAGE";
-
-export const READ_ONGLETS = "READ_ONGLETS";
-export const UPDATE_DIRECTION = "UPDATE_DIRECTION";
-export const ADD_STATION = "ADD_STATION"
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import onglets from "./ongletsReducer"
-import data from "./dataReducer"
-
-export default combineReducers({ onglets, data });
